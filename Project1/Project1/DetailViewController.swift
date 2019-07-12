9197bffbcae9f6336552204ee458df07d807a47e
Gregg McMillion <gmcmillion215@gmail.com>
2019-07-11 20:20:40 -0700

    Day 17, Project 1 Part 2

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Greggory Mcmillion on 7/11/19.
+//  Copyright © 2019 Greggory Mcmillion. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  @IBOutlet var imageView: UIImageView!
+  var selectedImage: String?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    title = selectedImage
+    navigationItem.largeTitleDisplayMode = .never
+    
+    if let imageToLoad = selectedImage {
+      imageView.image = UIImage(named: imageToLoad)
+    }
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.hidesBarsOnTap = true
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    navigationController?.hidesBarsOnTap = false
+  }
+}
